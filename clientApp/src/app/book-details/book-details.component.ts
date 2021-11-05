c4907ac9213317d628b5cfd58832726b2af2792e
sblevens <samiblevens@gmail.com>
2021-11-04 19:39:34 -0700

    add author and name to details page

--- clientApp/src/app/book-details/book-details.component.ts
+++ clientApp/src/app/book-details/book-details.component.ts
@@ -15,7 +15,7 @@ export class BookDetailsComponent implements OnInit {
   ngOnInit(): void {
     this.getId();
     this.getDisplayReviews();
-    
+    this.getDisplayName();
   }
 
 
@@ -30,45 +30,80 @@ export class BookDetailsComponent implements OnInit {
     this.id = Number(this.route.snapshot.paramMap.get('id'));
   }
 
+  getDisplayName(){
+    let name = this.all_reviews[this.id-1].name;
+    this.display_name.pop();
+    this.display_name.push(name);
+
+    let authors = this.all_reviews[this.id-1].authors;
+    this.display_author.pop();
+    this.display_author.push(authors);
+  }
+
   id: number = -1;
+  display_author = [
+    ['1']
+  ];
+  display_name = ['name'];
   display_reviews = [
     ['1']
   ];
 
   all_reviews = [
-    {id: 1,
+    {id: 1, name:'Frog and Toad',
       reviews: [
         'Frog and toad review', 'Such a good book', 'Love frog and toad.'
+      ],
+      authors: [
+        'author 1', 'author 2', 'author 3'
       ]
     },
-    {id: 2,
+    {id: 2, name:'Series of Unfortunate Events',
       reviews: [
         'Series of unfortunate events review', 'Such a good book', 'Love the movie.'
+      ],
+      authors: [
+        'author 1', 'author 2', 'author 3'
       ]
     },
-    {id: 3,
+    {id: 3, name:'Little Women',
       reviews: [
         'little women review', 'Such a good book', 'Love florence pugh.'
+      ],
+      authors: [
+        'author 1', 'author 2', 'author 3'
       ]
     },
-    {id: 4,
+    {id: 4, name:'Name of the Wind',
       reviews: [
         'name of the wind review', 'Such a good book', 'i dont know this book.'
+      ],
+      authors: [
+        'author 1', 'author 2', 'author 3'
       ]
-    },
-    {id: 5,
+    }, 
+    {id: 5, name:'Twilight',
       reviews: [
         'twilight review', 'Such a good book', 'Love kristen stewart.'
+      ],
+      authors: [
+        'author 1', 'author 2', 'author 3'
       ]
     },
-    {id: 6,
+    {id: 6, name:'Harry Potter',
       reviews: [
         'harry potter review', 'Such a good book', 'Love the movies.'
+      ],
+      authors: [
+        'author 1', 'author 2', 'author 3'
       ]
     },
-    {id: 7,
+    {id: 7, name:'Percy Jackson',
       reviews: [
         'percy jackson review', 'Such a good book', 'Love the movies.'
+      ],
+      authors: [
+        'author 1', 'author 2', 'author 3'
       ]
     },
   ]
